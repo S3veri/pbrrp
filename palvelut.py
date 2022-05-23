5fa1340fab32bf799d8f717572589be8e1b77e97
S3veri <82020949+S3veri@users.noreply.github.com>
2022-05-23 11:15:21 +0300

    Update palvelut.py

--- palvelut.py
+++ palvelut.py
@@ -22,56 +22,66 @@ class Asiakas:
         :type ika: int
         """
         self.__asiakasnumero = self.__luo_nro()
-        self.__nimi = nimi
-        self.__ika = ika
+        self.__nimi = str(nimi)
+        self.__ika = int(ika)
 
-    # Alla esitetty printtaa on vain omaa testausta varten; tämä siis otetaan lopullisesta versiosta pois
-    def printtaa(self):
-        print(self.__nimi)
-        print(self.__ika)
-        print(self.__asiakasnumero)
+    def get_nimi(self):
+        return self.__nimi
+    def get_ika(self):
+        return self.__ika
+    def set_nimi(self, uusinimi):
+        if uusinimi == False:
+            raise ValueError("Anna toinen nimi!")
+        if uusinimi == True:
+            self.__nimi = uusinimi
+    def set_ika(self, uusiika):
+        if uusiika == False:
+            raise ValueError("Anna toinen ika!")
+        if  uusiika == True:
+            self.__ika = uusiika
 
 class Palvelu(Asiakas):
     def __init__(self, tuotenimi):
-        asiakkaat = []
+        self.asiakkaat = []
         
-    def lisaa_asiakas(Asiakas):
+    def lisaa_asiakas(self, Asiakas):
         self.asiakkaat.append(Asiakas)
         if Asiakas == False:
             raise ValueError("Anna toinen asiakas")
         
-    def poista_asiakas(Asiakas):
-        self.asiakkaat.pop(Asiakas)
-        if Asiakas == False:
-
-    def tulosta_asiakkaat():
-        for x in range(asiakkaat):
-            
-            
-        pass
+    def poista_asiakas(self, Asiakas):
+        try:
+            if Asiakas == False:
+                raise ValueError("Anna uusi asiakas")
+            else:
+                self.asiakkaat.pop(Asiakas)
+        except:
+            pass
+    def tulosta_asiakkaat(self):
+            pass
     
-    def luo_asiakasrivi():
-        merkkijono = 
-        return 
+    def luo_asiakasrivi(self):
         pass
 
 class ParempiPalvelu(Palvelu):
     def __init__(self, tuotenimi):
-        edut = []
-    def lisaa_etu(Etu):
+        super().__init__(self)
+        self.edut = []
+    def lisaa_etu(self, Etu):
         self.edut.append(Etu)
         if Etu == False:
             raise ValueError("Anna toinen Etu")
-    def poista_etu(Etu):
-        self.edut.pop(Etu)
-        if Etu == False:
+    def poista_etu(self, Etu):
+        try:
+            if Etu == False:
+                raise ValueError("Anna uusi etu")
+            else:
+                self.edut.pop(Etu)
+        except:
             pass
-    def tulosta_edut():
+            
+    def tulosta_edut(self):
         pass
     pass
 
-
-# Alla esitetty on testaa yllä mainitut
-koe = Asiakas('Ville', 12)
-koe.printtaa()
 
